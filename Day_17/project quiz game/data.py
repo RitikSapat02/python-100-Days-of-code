9686e4072861c18884b56dd2a8df55350f0d6942
Ritik Sapat <118529770+RitikSapat02@users.noreply.github.com>
2022-12-31 22:42:09 +0530

    Add files via upload

---
+++ Day_17/project quiz game/data.py
@@ -0,0 +1,102 @@
+question_data = [
+{
+"category": "Geography",
+"type": "boolean",
+"difficulty": "easy",
+"question": "A group of islands is called an &#039;archipelago&#039;.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"category": "Geography",
+"type": "boolean",
+"difficulty": "easy",
+"question": "The Republic of Malta is the smallest microstate worldwide.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"category": "Geography",
+"type": "boolean",
+"difficulty": "easy",
+"question": "San Marino is the only country completely surrounded by another country.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"category": "Geography",
+"type": "boolean",
+"difficulty": "easy",
+"question": "There are no deserts in Europe.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"category": "Geography",
+"type": "boolean",
+"difficulty": "easy",
+"question": "Toronto is the capital city of the North American country of Canada.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"category": "Geography",
+"type": "boolean",
+"difficulty": "easy",
+"question": "California is larger than Japan.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"category": "Geography",
+"type": "boolean",
+"difficulty": "easy",
+"question": "Hungary is the only country in the world beginning with H.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"category": "Geography",
+"type": "boolean",
+"difficulty": "easy",
+"question": "New Haven is the capital city of the state of Connecticut in the United States.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"category": "Geography",
+"type": "boolean",
+"difficulty": "easy",
+"question": "Rhode Island is actually located on the US mainland, despite its name.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"category": "Geography",
+"type": "boolean",
+"difficulty": "easy",
+"question": "There is an island in Japan called Ōkunoshima, A.K.A. &quot;Rabbit Island&quot;, so named because of it&#039;s huge population of rabbits.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+}
+]
